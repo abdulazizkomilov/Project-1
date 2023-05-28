3db2b9067b9ec86abbc8ac34b2b3034586be5666
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-05-28 14:51:13 +0500

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -15,4 +15,7 @@ urlpatterns = [
     path('profile/favourites/', views.favourite_list, name='favourite_list'),
     path('profile/removed/', views.removed_list, name='removed_list'),
     path('fav/<int:id>/', views.favourite_add, name='favourite_add'),
+    path('like/<int:id>/', views.like_dislike, name='like_dislike'),
+    path('follow/<int:id>/', views.follow, name='follow'),
+    path('unfollow/<int:id>/', views.unfollow, name='unfollow'),
 ]
